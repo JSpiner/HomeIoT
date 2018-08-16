53cbbbff4aecf9878ceb5d24f97810b53ada291e
JSpiner <jspiner@naver.com>
2018-08-16 22:58:11 +0900

    #3 아두이노로부터 init데이터를 받는 url 추가

--- home/app.py
+++ home/app.py
@@ -7,4 +7,14 @@ app = Flask(__name__)
 def routeGetRoot():
     return 'ok'
 
+@app.route('/init')
+def routeGetInit():
+    deviceIp = request.form['ip']
+    deviceType = request.form['type']
+    deviceName = request.form['name']
+
+    print ("ip : " + str(deviceIp))
+    print ("type : " + str(deviceType))
+    print ("name : " + str(deviceName))
+
 app.run(debug=True, host='0.0.0.0')
